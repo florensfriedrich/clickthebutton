159cae099e9b8a0649824e76c772fdba8280b847
florensfriedrich <77543367+florensfriedrich@users.noreply.github.com>
2021-01-16 19:08:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+/*
+let name ='Fuck'; //String
+let age = 18; //Number
+let isApproved = true; //Boolean
+let firstName = undefined; //undefined
+let lastName = null; //null (to clear a value of a variable)
+
+
+let object1 = {
+    age: 18,
+    name: 'Florens'
+};
+
+object1.name = 'Cunt';
+
+let selection = 'name';
+object1[selection] = 'Peter';
+
+console.log(object1.name);
+
+
+let selectedColours = ['red', 'blue'];//array
+selectedColours[2] = 1; //add things to array
+console.log(selectedColours.length);
+
+
+function newFunction(name, lastName) { //function 
+    console.log('Hello ' + name + ' ' + lastName); //body of a function
+}
+
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
+
+
+
+alert("Bruh y tf are you on this site?");
+
+let arrayTest = [1, 2, 3];
+
+function changeArray() {
+    arrayTest[3] = 4;
+}
+changeArray;
+console.log(arrayTest);
+
+*/
+
+function newFunction() {
+  alert("How are you today?");
+}
+
+function otherFunction() {
+  for (let i = 0; i < 100; i++) {
+    alert("CLICK THE BUTTON.");
+  }
+}
